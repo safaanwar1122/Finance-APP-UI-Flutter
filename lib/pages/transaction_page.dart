39f5019d36f1262443e2b3682a052404051d4bdf
Safa <safaanwar2211@gmail.com>
2023-08-17 20:52:12 -0700

    2 commit

--- lib/pages/transaction_page.dart
+++ lib/pages/transaction_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -38,7 +37,7 @@ class _TransectionPageState extends State<TransectionPage> {
                 ],
               ),
               padding:
-              EdgeInsets.only(top: 20, bottom: 25, right: 20, left: 20),
+                  EdgeInsets.only(top: 20, bottom: 25, right: 20, left: 20),
               child: Column(
                 children: [
                   Row(
@@ -56,11 +55,249 @@ class _TransectionPageState extends State<TransectionPage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-
+                        Text(
+                          "Recent Transaction",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: mainFontColor,
+                          ),
+                        ),
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 13,
+                            color: mainFontColor,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 5,
+                      bottom: 20,
+                      left: 25,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: 25, right: 25, top: 5, bottom: 5),
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            color: buttoncolor,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'All',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: 25, right: 25, top: 5, bottom: 5),
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: white,
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: grey.withOpacity(.03),
+                                  spreadRadius: 10,
+                                  blurRadius: 3),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Income',
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(.5),
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: 25, right: 25, top: 5, bottom: 5),
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: white,
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: grey.withOpacity(.03),
+                                  spreadRadius: 10,
+                                  blurRadius: 3),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Expense',
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(.5),
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 25, right: 25, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Today",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: mainFontColor),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    left: 25,
+                    right: 25,
+                  ),
+                  decoration: BoxDecoration(
+                    color: white,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: [
+                      BoxShadow(
+                        color: grey.withOpacity(0.03),
+                        spreadRadius: 10,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: 10, bottom: 20, right: 20, left: 20),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          child: Center(
+                            child: Icon(
+                              Icons.payment,
+                              color: mainFontColor,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                            child: Container(
+                          width: (size.width - 90) * .7,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Payment',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                'Paymnet from Safa Anwar',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: black.withOpacity(0.5),
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        )),
+                        Expanded(
+                            child: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                '\$30.00',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: black,
+                                ),
+                              )
+                            ],
+                          ),
+                        ))
+                      ],
+                    ),
+                  ),
+                ))
+              ],
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(top: 20, bottom: 10, right: 20, left: 20),
+              child: Column(
+                children: [
+                  Container(
+                    padding:
+                        EdgeInsets.only(left: 25, right: 25, top: 5, bottom: 5),
+                    margin: EdgeInsets.symmetric(horizontal: 15),
+                    child: Container(
+                      width: 325,
+                      height: 325,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://res.cloudinary.com/dic3o7vzw/image/upload/v1673927487/avatars/cropped_uxagcn.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.all(25),
+              decoration: BoxDecoration(
+                color: buttoncolor,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Center(
+                child: Text(
+                  "See Details",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600),
+                ),
               ),
             ),
           ],
